157fdd02f6cae0ea979457b377749608226ba9f2
mdivk <mdivk@users.noreply.github.com>
2018-06-02 11:43:46 -0400

    Multi-threading added and pending finalized

--- index/rindex.py
+++ index/rindex.py
@@ -1,4 +1,8 @@
+import os
+import sys
 from optparse import OptionParser
+from time import sleep
+from threading import Thread
 
 # Usage:
 # rindex.py -T job/flow_name.txt -D job/flow_days.txt
@@ -6,6 +10,7 @@ from optparse import OptionParser
 
 SOLR_URL = ''
 POST_JAR_URL = 'opt/cloudera/parcels/CDH/jars/post.jar'
+MAX_THREADS = 10
 
 single_day = ''
 solr_server = ''
@@ -15,14 +20,19 @@ json_loc = ''
 index_command_base = 'java -Dtype=application/json -Drecursive -Durl='
 index_command = ''
 solr_instance_detail = ''
+flow_name = ''
+flow_days = ''
 flow_name_loc = ''
+json_list = []
+
 
 class rindex():
     def __init__(self, solr_server=None, collection=None, flow_name=None, flow_days=None):
         self.json_file = ''
         self.process_options(solr_server, collection, flow_name, flow_days)
 
-    def process_options(self, solr_server, collection, flow_name, flow_days):
+    def process_options(self, the_solr_server, the_collection, the_flow_name, the_flow_days):
+
         parser = OptionParser()
         parser.add_option("-s", "--solr_instance_detail", dest="solr_instance_detail",
                           help="Enter solr_instance_detail")
@@ -33,30 +43,41 @@ class rindex():
         (options, args) = parser.parse_args()
 
         if not options.solr_instance_detail:
-            if not solr_server:
+            if not the_solr_server:
                 parser.error('solr_server not provided (-s solr_server)')
         else:
             with open(options.solr_instance_detail) as f:
-                the_solr_instance_detail = f.readlines()
-            solr_server = the_solr_instance_detail[0].split('|')[0]
-            collection = the_solr_instance_detail[0].split('|')[1]
+                solr_instance_detail = f.readlines()
+
+            global solr_server
+            global collection
+            solr_server = solr_instance_detail[0].split('|')[0]
+            collection = solr_instance_detail[0].split('|')[1]
 
         if not options.flow_name:
-            if not flow_name:
+            if not the_flow_name:
                 parser.error('flow name not provided (-f option)')
+        else:
+            global flow_name
+            flow_name = options.flow_name
 
         if not options.flow_days:
-            if not flow_days:
+            if not the_flow_days:
                 parser.error('flow days not provided (-d option)')
+        else:
+            global flow_days
+            flow_days = options.flow_days
 
-        # self.options = options
-        self.read_flow_days(solr_server, collection, options.flow_name, options.flow_days)
+    def get_json_files(self, json_loc):
+        '''
+        Get list of json files in json_loc
+        '''
+        files_list = []
 
-    def read_file(self, filename):
-        try:
-            f = open(filename, 'r')
-        except:
-            self.logger.error('Error while reading ' + filename)
+        for x in os.walk(json_loc):
+            files_list.append(x)
+
+        return files_list
 
     def single_rindex(self, solr_server, collection, flow_name, flow_days, json_loc):
 
@@ -65,39 +86,69 @@ class rindex():
         SOLR_URL = 'http://' + solr_server + '.nam.nsroot.net:8983/solr/' + collection + '/update/json/docs'
         # reaad entries from flow_days
         index_command = index_command_base + SOLR_URL + ' -jar ' + POST_JAR_URL + ' ' + single_day + ' ' + json_loc
-
         print(index_command)
         # os.system(single_command)
 
     def read_flow_name(self, flow_name):
         global flow_name_loc
         with open(flow_name) as f:
-            r = f.readlines()
+            c = f.readlines()
             # you may also want to remove whitespace characters like `\n` at the end of each line
-        r = [x.strip() for x in r]
-        flow_name_loc = r[0]
+        c = [x.strip() for x in c]
+        flow_name_loc = c[0]
+
+        return flow_name_loc
 
     def read_flow_days(self, solr_server, collection, flow_name, flow_days):
         with open(flow_days) as f:
-            content = f.readlines()
+            c = f.readlines()
         # you may also want to remove whitespace characters like `\n` at the end of each line
-        content = [x.strip() for x in content]
+        flow_days = [x.strip() for x in c]
 
-        self.read_flow_name(flow_name)
-        print(flow_name_loc)
+        return flow_days
 
+    def worker_func(self, solr_server, collection, flow_name, flow_days, json_loc):
+        sys.stdout.write("\rProcessing file " + str(i + 1) + "/" + self.total_files + '...')
+        sys.stdout.flush()
 
-        # now we have the date for the json files to be indexed
-        for i, each_date in enumerate(content):
-            json_loc = json_loc_base + flow_name_loc + '/json/' + each_date
-            # print(json_loc)
-            self.single_rindex(solr_server, collection, flow_name, flow_days, json_loc)
-        return i + 1
+        self.single_rindex(solr_server, collection, flow_name, flow_days, json_loc)
 
 
     def run(self):
+        # self.options = options
+
+        # now we have the date for the json files to be indexed
+
+        cur_flow_days = []
+        cur_flow_days = self.read_flow_days(solr_server, collection, flow_name, flow_days)
+
+        for i, each_date in enumerate(cur_flow_days):
+            # for each flow_day, start a Thread to process it, max is 10
+            json_loc = json_loc_base + flow_name_loc + '/json/' + each_date
+            # print(json_loc)
+            self.single_rindex(solr_server, collection, flow_name, flow_days, json_loc)
+
         print('\nDone!')
 
+
+'''     
+The following code is for MultiThreading, only to be uncommented after the single Thread is working as expected:
+   
+        for i, each_date in enumerate(cur_flow_days):
+            threads = [a for a in threads if a.isAlive()]
+
+            while len(threads) >= MAX_THREADS:
+                sleep(0.1)
+                threads = [a for a in threads if a.isAlive()]
+
+            t = Thread(target=self.worker_func, args=(input_f, i))
+            threads.append(t)
+            # self.logger.debug('Number of active threads: ' + str(len([a for a in threads if a.isAlive()])))           
+            t.start()
+
+        for t in threads:
+            t.join()
+'''
 
 if __name__ == '__main__':
     rindex().run()
