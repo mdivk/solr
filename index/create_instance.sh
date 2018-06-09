d74b4c683db20d84a4e247af0cc199d1e9cb7bd2
mdivk <mdivk@users.noreply.github.com>
2018-06-08 23:01:32 -0400

    Update create_instance.sh

--- index/create_instance.sh
+++ index/create_instance.sh
@@ -2,9 +2,9 @@
 #Created on: 20180529
 #Author: RX
 #Usage:
-#to be exected on Proxy server
+#to be executed on Proxy server
 #./create_instance.sh <collection_name>
-solr_dir = "usr/indexer/instance"
+solr_dir="/usr/indexer/instance"
 echo "solrctl instancedir --generate $solr_dir/$1"
 echo "solrctl instancedir --create $1 $solr_dir/$1"
 echo "solrctl collection --create $1 -s 1 -r 2"
@@ -12,3 +12,4 @@ echo "mv $solrsolr_dir/conf/schema.xml $solrsolr_dir/conf/schema~.xml"
 echo "cp /usr/indexer/schema/schema_citifx.xml $solr_dir/$1/conf/schema.xml"
 echo "solrctl instancedir --update indexer_demo_fix  $solr_dir/$1"
 echo "solrctl collection --reload  $1"
+
