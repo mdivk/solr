5c4c7f5397515cff3867d0cf194f4bb93bda56ea
mdivk <mdivk@users.noreply.github.com>
2018-06-08 23:11:02 -0400

    Update delete_instance.sh

--- index/delete_instance.sh
+++ index/delete_instance.sh
@@ -3,5 +3,7 @@
 #Author: RX
 #Usage:
 #./delete_instance.sh <solr_hostname> <collection_name> 
-echo "http://"$1".nam.nsroot.net:8983/solr/admin/collections?action=DELETE&name=$2"
+echo "curl http://"$1.nam.nsroot.net:8983/solr/admin/collections?action=DELETE&name=$2
+echo "solrctl instancedir --delete $2"
+echo "rm -r /usr/indexer/instance/$2"
 
